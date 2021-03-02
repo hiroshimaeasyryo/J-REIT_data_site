ba64d58f3659c4960045292645e04b13f62fce3f
hiroshimaeasyryo <higahigahigahigahigahiga@gmail.com>
2021-03-02 17:22:38 +0900

    add jreit analysys

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 import pandas as pd
+import plotly.express as px
+import plotly.graph_objs as go
+from plotly.subplots import make_subplots
+from bs4 import BeautifulSoup
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import datetime
+import itertools
+from sklearn import linear_model
 import statsmodels.api as sm
 
 
@@ -51,6 +57,34 @@ for d in osk_daily_cases.index:
 osk_daily_cases.index = osk_datetimes
 osk_seasonal = sm.tsa.seasonal_decompose(osk_daily_cases.counts, period=7)
 osk_daily_cases['trend'] = osk_seasonal.trend
+
+# J-REITデータ
+# βとαの値を取得して表示する
+def calc_beta_alpha(x, y):
+    clf = linear_model.LinearRegression()
+    clf.fit(x, y)
+    print("β = ", list(itertools.chain.from_iterable(clf.coef_)))
+    print("α = ", clf.intercept_)
+
+# 対象銘柄のticker code
+ticker = "8951"
+
+# 東証REIT指数のデータを取得
+reitsheet = pd.read_html(
+    "https://finance.matsui.co.jp/Stocks/matsui/contents/stockDetail.aspx?cntcode=JP&skubun=0&stkcode=0075&exctype=01&tab=2&pnum=1Y&ptype=W#",
+    flavor='bs4')[0]
+reit_w_udratio = reitsheet[::-1]['終値'].pct_change().dropna() * 100
+
+# 対象銘柄のデータを取得
+target_sheet = pd.read_html(
+    "https://finance.matsui.co.jp/Stocks/matsui/contents/stockDetail.aspx?cntcode=JP&skubun=1&stkcode=" + ticker + "&exctype=01&tab=3&pnum=1Y&ptype=W#'",
+    flavor='bs4')[0]
+target_udratio = target_sheet[::-1]['終値'].pct_change().dropna() * 100
+
+# 単回帰分析
+ud_df = pd.DataFrame({"J-REIT Index": reit_w_udratio,
+                      "ticker" + ticker: target_udratio})
+
 
 # Layout
 app.layout = html.Div(
@@ -104,8 +138,23 @@ app.layout = html.Div(
                 'layout': {'title': '大阪府内新規感染者数'}
                     }
                 )
-            ]
+            ,
+        html.H1(children='J-REIT Index α & β'),
+                dcc.Graph(
+                    id='graph4',
+                    figure=px.scatter(
+                        ud_df,
+                        x="J-REIT Index",
+                        y="ticker" + ticker,
+                        marginal_y="violin",
+                        marginal_x="box",
+                        trendline="ols",
+                        template="simple_white"
+                    )
+                ),
+    ]
 )
+
 
 server = app.server
 
