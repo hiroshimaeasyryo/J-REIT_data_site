f72e2c876fb71b68071d2d2967a5cc5a90e62269
hiroshimaeasyryo <higahigahigahigahigahiga@gmail.com>
2021-03-23 10:21:47 +0900

    change redis url

--- worker.py
+++ worker.py
@@ -4,7 +4,7 @@ from rq import Worker, Queue, Connection
 
 
 listen = ['high', 'default', 'low']
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+redis_url = os.getenv('REDISTOGO_URL', 'redis://redistogo:621717682c6f059a39f1750f1a52b236@hammerjaw.redistogo.com:11524/')
 conn = redis.from_url(redis_url)
 
 
